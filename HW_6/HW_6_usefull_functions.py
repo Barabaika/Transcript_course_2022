fee7eec86e4281d648a95eed84da6babd5623489
barabaika <shevtsov.av@phystech.edu>
2022-10-19 10:48:57 +0000

    HW_6 sc usefull functions

---
+++ HW_6/HW_6_usefull_functions.py
@@ -0,0 +1,65 @@
+'''
+Задание:
+Сделайте несколько вспомогательных функций, которые вам позднее пригодятся:
+'''
+
+import numpy as np
+import scanpy as sc
+import rpy2.robjects as robjects
+
+'''
+1- Разбиение одного объекта AnnData на список других, соответствующих каждому индивидуальному батчу;
+'''
+
+def split_by_batch(adata, meta_data):
+  '''
+  func to split anndata object based on meta data in anndata.obs
+  inputs:
+    adata: Anndata object
+    meta_data: name of column in adata.obs to split by it
+  outputs:
+    res_dict: {meta_name1: adata, meta_name2: adata, ... , meta_nameN: adata} 
+  '''
+  res_dict = {}
+  for group in adata.obs[meta_data].unique():
+    
+    res_dict[group] = adata[adata.obs[meta_data] == group, :]
+  return res_dict
+
+
+'''
+2- Создание нового объекта со случайными N клетками и M генами из изначального;
+'''
+
+def random_sample_from_adata(adata, n, m, random_state = 42):
+  '''
+  func to make a subsample from adata with random N cells and m genes 
+  inputs:
+    adata: Anndata object
+    n: int, number of random cells
+    m: int, number of random genes
+  outputs:
+    sub_adata
+  '''
+  R = np.random.RandomState(random_state)
+  sub_adata = adata[
+    R.randint(0, adata.shape[0], n),
+    R.randint(0, adata.shape[0], m)
+  ]
+  return sub_adata
+
+'''
+3- Сохранение вашего AnnData-объекта как Seurat-объекта в формате .rds.
+'''
+def save_adata_as_rds(adata, save_path):
+  '''
+  func to save adata object as .rds file 
+  inputs:
+    adata: Anndata object
+    save_path: str, <path_to_save>/<file_name>.rds
+  outputs:
+    - 
+  '''
+  # возможно нужно чначала сделать seurat object а потом сохранять 
+  saveRDS = robjects.r['saveRDS']
+  saveRDS(adata, 'del.rds')
